9e14a580452337534986d8c40918774a732cb843
MightyBeaver <bobrnah486@gmail.com>
2017-10-31 15:40:17 +0300

    Prepare for HW3

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -1,19 +1,21 @@
+package ru.javawebinar.basejava.storage;
+
 import java.util.Arrays;
+
+import ru.javawebinar.basejava.model.Resume;
 
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
-    private int actualSize = 0;
+public class ArrayStorage extends AbstractArrayStorage {
 
     public void clear() {
         Arrays.fill(storage,null);
-        actualSize = 0;
+        size = 0;
     }
 
-    private int getMatchIndex(String uuid) {
-        for (int i = 0; i < actualSize; i++) {
+    protected int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
             }
@@ -22,21 +24,21 @@ public class ArrayStorage {
     }
 
     public void save(Resume r) {
-        int matchIndex = getMatchIndex(r.getUuid());
+        int matchIndex = getIndex(r.getUuid());
         if (matchIndex != -1) {
             System.out.println("This resume is already in storage");
             return;
         }
-        if (actualSize == storage.length) {
+        if (size == storage.length) {
             System.out.println("Storage at full capacity");
             return;
         }
-        storage[actualSize] = r;
-        actualSize++;
+        storage[size] = r;
+        size++;
     }
 
     public void update(Resume r) {
-        int matchIndex = getMatchIndex(r.getUuid());
+        int matchIndex = getIndex(r.getUuid());
         if(matchIndex == -1) {
             System.out.println("No such resume in storage");
             return;
@@ -45,7 +47,7 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        int matchIndex = getMatchIndex(uuid);
+        int matchIndex = getIndex(uuid);
         if (matchIndex == -1) {
             return null;
         }
@@ -53,29 +55,24 @@ public class ArrayStorage {
     }
 
     public void delete(String uuid) {
-        int matchIndex = getMatchIndex(uuid);
+        int matchIndex = getIndex(uuid);
         if (matchIndex == -1) {
             System.out.println("No such resume in storage");
             return;
         }
-        storage[matchIndex] = storage[actualSize -1];
-        storage[actualSize - 1] = null;
-        actualSize--;
+        storage[matchIndex] = storage[size -1];
+        storage[size - 1] = null;
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        if (actualSize > 0) {
-            Resume[] allElements = new Resume[actualSize];
-            allElements = Arrays.copyOfRange(storage, 0, actualSize);
-            return allElements;
-        }
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     public int size() {
-        return actualSize;
+        return size;
     }
 }
