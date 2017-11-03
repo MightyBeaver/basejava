99bfa188767f27075c07c303835381c6bfd9be85
MightyBeaver <bobrnah486@gmail.com>
2017-11-03 17:33:55 +0300

    invoke toString() in Resume via reflection. Add lib to .gitignore

---
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException,
+            InvocationTargetException {
+        Resume r = new Resume();
+        Class resumeReflection = r.getClass();
+        Field field = resumeReflection.getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
+        Method m = resumeReflection.getDeclaredMethod("toString",null );
+        System.out.println(m.getName());
+        System.out.println(m.invoke(r));
+    }
+}
